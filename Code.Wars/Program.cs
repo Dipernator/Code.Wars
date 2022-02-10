94ac38279a15d739684269132e14569e10fb25cf
Dipernator <tamotloung@hotmail.co.za>
2022-02-10 13:58:01 +0200

    Add project files.

---
+++ Code.Wars/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Wars
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WellOfIdeasEasyVersion wellOfIdeasEasyVersion = new WellOfIdeasEasyVersion();
+            wellOfIdeasEasyVersion.Well(new string[] { "good", "bad", "bad", "bad", "bad" });
+
+        }
+    }
+}
---
+++ Code.Wars/WellOfIdeasEasyVersion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Wars
+{
+    /// <summary>
+    /// For every good kata idea there seem to be quite a few bad ones! In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. 
+    /// If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+    /// https://www.codewars.com/kata/57f222ce69e09c3630000212/train/csharp
+    /// </summary>
+    public class WellOfIdeasEasyVersion
+    {
+        public string Well(string[] srtArr)
+        {
+            srtArr.Where(m => m.Contains("good")).ToString().Count();
+            return "";
+        }
+    }
+}
